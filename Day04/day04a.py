ec656270dc2cd5a606bef1615cd42300f8447b35
RobertJN64 <robertjnies@gmail.com>
2022-12-04 00:09:18 -0500

    top 300 in day 4 with decent solution

--- Day04/day04a.py
+++ Day04/day04a.py
@@ -1,4 +1,26 @@
 def main():
     with open("Day04/day04.txt") as f:
         lines = f.readlines()
-    print(lines[0:10])
+
+    counter = 0
+    for line in lines:
+        a, b = line.strip().split(",")
+        aS, aE = map(int, a.split("-"))
+        bS, bE = map(int, b.split("-"))
+
+        found = False
+        for item in list(range(aS, aE + 1)):
+            if item not in list(range(bS, bE + 1)):
+                break
+        else:
+            counter += 1
+            found = True
+
+        if not found:
+            for item in list(range(bS, bE + 1)):
+                if item not in list(range(aS, aE + 1)):
+                    break
+            else:
+                counter += 1
+
+    print(counter)
--- Day04/day04b.py
+++ Day04/day04b.py
@@ -1,4 +1,24 @@
 def main():
     with open("Day04/day04.txt") as f:
         lines = f.readlines()
-    print(lines[0:10])
+
+    counter = 0
+    for line in lines:
+        a, b = line.strip().split(",")
+        aS, aE = map(int, a.split("-"))
+        bS, bE = map(int, b.split("-"))
+
+        found = False
+        for item in list(range(aS, aE + 1)):
+            if item in list(range(bS, bE + 1)):
+                counter += 1
+                found = True
+                break
+
+        if not found:
+            for item in list(range(bS, bE + 1)):
+                if item in list(range(aS, aE + 1)):
+                    counter += 1
+                    break
+
+    print(counter)
