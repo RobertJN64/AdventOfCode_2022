983bf9ef6deb974c0e45a670aa527e8d10676ba4
RobertJN64 <robertjnies@gmail.com>
2022-12-05 00:14:34 -0500

    top 500 with literal solution

--- Day05/day05a.py
+++ Day05/day05a.py
@@ -1,4 +1,32 @@
 def main():
+    with open("Day05/day05pic.txt") as f:
+        lines = f.readlines()
+
+    lcount = len(lines[-1].strip().split("   "))
+    cols = []
+    for item in range(0, lcount):
+        cols.append([])
+
+    lines = lines[:-1]
+    lines.reverse()
+
+    for line in lines:
+        line = line.removesuffix('\n')
+        for gindex in range(0, len(line), 4):
+            if line[gindex+1:gindex+2] != " ":
+                cols[int(gindex/4)].append(line[gindex+1:gindex+2])
+
     with open("Day05/day05.txt") as f:
         lines = f.readlines()
-    print(lines[0:10])
+
+    for line in lines:
+        cmds = line.split()
+        count = int(cmds[1])
+        start = int(cmds[3]) - 1
+        end = int(cmds[5]) - 1
+
+        for i in range(0, count):
+            cols[end].append(cols[start].pop())
+
+    for item in cols:
+        print(item[-1], end='')
---
+++ Day06/day06b.py
@@ -0,0 +1,4 @@
+def main():
+    with open("Day06/day06.txt") as f:
+        lines = f.readlines()
+    print(lines[0:10])
