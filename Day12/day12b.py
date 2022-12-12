4dedca5d0f060dfb580ca77b632291a91c02da52
RobertJN64 <robertjnies@gmail.com>
2022-12-12 00:22:06 -0500

    Day12: Very efficient maze solver with work backwards approach

--- Day12/day12b.py
+++ Day12/day12b.py
@@ -1,4 +1,39 @@
 def main():
     with open("Day12/day12.txt") as f:
         lines = f.readlines()
-    print(lines[0:10])
+    end_pos = (0,0)
+    grid = []
+    costs = []
+
+    for index, line in enumerate(lines):
+        grid.append(list(map(ord, line.strip().replace("S", "a").replace("E", "z"))))
+        costs.append([float('inf')] * len(grid[0]))
+        if "E" in line:
+            end_pos = (line.index("E"), index)
+
+    costs[end_pos[1]][end_pos[0]] = 0
+
+    changed = True
+    while changed:
+        changed = False
+        for y in range(0, len(grid)):
+            for x in range(0, len(grid[y])):
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    n_x = x + dx
+                    n_y = y + dy
+                    if 0 <= n_x < len(grid[0]) and 0 <= n_y < len(grid):
+                        if costs[n_y][n_x] + 1 < costs[y][x]:
+                            if grid[n_y][n_x] - grid[y][x] <= 1:
+                                costs[y][x] = costs[n_y][n_x] + 1
+                                changed = True
+
+    best_cost = float('inf')
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid[y])):
+            if grid[y][x] == ord('a'):
+                best_cost = min(best_cost, costs[y][x])
+    print(best_cost)
+
+
+if __name__ == '__main__':
+    main()
