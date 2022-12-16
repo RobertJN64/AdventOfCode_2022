43f2c9b4ae9941a911157af54ba65de425bb0366
RobertJN64 <robertjnies@gmail.com>
2022-12-16 15:28:04 -0500

    day 16 working on example case

--- Day16/day16a.py
+++ Day16/day16a.py
@@ -1,6 +1,5 @@
 import copy
 
-
 class Valve:
     def __init__(self, line):
         line = line.replace(",", "").replace("=", " ").replace(";", "").strip().split(" ")
@@ -22,22 +21,32 @@ class Tunnel:
 
 class Q:
     def __init__(self):
+        #CORE INFO
         self.location = 'AA'
         self.timer = 0
         self.open_valves = []
         self.total_flow_rate = 0
         self.active_flow_rate = 0
-        self.last_loc = ''
 
+        #PRUNING INFO
         self.location_db = {'AA': 0}
-        self.turn_time = []
+
+        #DEBUG INFO
+        self.valve_time = []
         self.flow_hist = []
+        self.loc_hist = []
 
-    def update(self):
+    def update_flow_counter(self):
         self.total_flow_rate += self.active_flow_rate
-        self.location_db[self.location] = self.active_flow_rate
         self.flow_hist.append(self.active_flow_rate)
+        self.location_db[self.location] = self.active_flow_rate
+        self.loc_hist.append(self.location)
+        self.timer += 1
 
+    def open_valve(self, valve_db):
+        self.open_valves.append(self.location)
+        self.valve_time.append(self.timer) #MATCH AoC info
+        self.active_flow_rate += valve_db[self.location].flow_rate
 
 def main():
     with open("Day16/day16.txt") as f:
@@ -48,11 +57,7 @@ def main():
         v = Valve(line)
         valve_db[v.id] = v
 
-    # METHOD: 0 point compression
-    # Goal: Create optimized datastructure of paths
-
     #STEP 1: Convert valve_db into tunnel list
-
     tunnels = []
     for v in valve_db.values():
         for path in v.paths:
@@ -64,7 +69,6 @@ def main():
                 tunnels.append(Tunnel(v.id, path, 1))
 
     #STEP 2: Combine tunnels with 0 flow rate middle and exactly 2 exits
-
     for v in valve_db.values():
         if v.flow_rate == 0 and len(v.paths) == 2:
             t_comb_list = []
@@ -83,77 +87,51 @@ def main():
             t_shared_points = list(t_shared_points)
             tunnels.append(Tunnel(t_shared_points[0], t_shared_points[1], t_comb_list[0].length + t_comb_list[1].length))
 
+    print(len(tunnels), tunnels)
 
-    print(tunnels)
     queue = [Q()]
-
-    correct_v = ['DD', 'BB', 'JJ', 'HH', 'EE', 'CC']
-    correct_t = [1, 4, 8, 16, 20, 23]
-
-    best_flow = 1000
-    def add_q(_qt: Q):
+    best_flow = 1600
+    def add_q(qt: Q):
         nonlocal best_flow
-        if _qt.timer >= 29:
+        if qt.timer >= 30:
             if qt.total_flow_rate > best_flow:# and str(qt.open_valves) == str(correct_v) and str(qt.turn_time) == str(correct_t):
-                print("Found solution with flow rate: ", qt.total_flow_rate, qt.active_flow_rate, qt.open_valves, qt.turn_time, qt.flow_hist)
+                print("Found solution with flow rate: ", qt.total_flow_rate, qt.active_flow_rate, qt.open_valves, qt.valve_time, qt.flow_hist, qt.loc_hist, sep='\n')
+                print()
+                print()
                 best_flow = qt.total_flow_rate
         else:
-            queue.append(_qt)
+            queue.append(qt)
 
-    #STEP 3 - search through tunnels
-    counter = 0
     while queue:
         q = queue.pop()
-        counter += 1
-        if counter%10000 == 0:
-            print(q.timer, len(queue))
-        q.update()
+
         if q.location not in q.open_valves and valve_db[q.location].flow_rate != 0:
-            qt = copy.deepcopy(q)
-            qt.open_valves.append(q.location)
-            qt.turn_time.append(qt.timer)
-            qt.active_flow_rate += valve_db[q.location].flow_rate
-            qt.timer += 1
-            qt.last_loc = '' # we can return anywhere now
-            add_q(qt)
+            def open_valve():
+                qt = copy.deepcopy(q)
+                qt.update_flow_counter()
+                qt.open_valve(valve_db)
+                add_q(qt)
+            open_valve()
 
         for t in tunnels:
-            if t.point_a == q.location and t.point_b != q.last_loc:
-                qt = copy.deepcopy(q)
-                qt.last_loc = q.location
-                qt.timer += t.length
-
-                for i in range(t.length - 1):
-                    qt.update()
-                qt.location = t.point_b
-
-                if qt.location not in qt.location_db:
-                    add_q(qt)
-                elif qt.location_db[qt.location] != qt.active_flow_rate:
+            def check_travel(current, target):
+                if current == q.location and (target not in q.location_db or q.active_flow_rate > q.location_db[target]):
+                    if q.timer + t.length > 30:
+                        return
+                    qt = copy.deepcopy(q)
+                    for _ in range(t.length):
+                        qt.update_flow_counter()
+                    qt.location = target
                     add_q(qt)
 
-            if t.point_b == q.location and t.point_a != q.last_loc:
-                qt = copy.deepcopy(q)
-                qt.last_loc = q.location
-
-                qt.timer += t.length
-
-                for i in range(t.length - 1):
-                    qt.update()
-                qt.location = t.point_a
-
-                if qt.location not in qt.location_db:
-                    add_q(qt)
-                elif qt.location_db[qt.location] != qt.active_flow_rate:
-                    add_q(qt)
+            check_travel(t.point_a, t.point_b)
+            check_travel(t.point_b, t.point_a)
 
         #OPTION - SKIP TO END
-        qt = copy.deepcopy(q)
-        r_time = 29 - qt.timer
-        qt.timer += r_time
-        for _ in range(r_time):
-            qt.update()
-            #qt.total_flow_rate += qt.active_flow_rate * r_time
-        add_q(qt)
-
-        #IMPROPERLY CALC FLOW RATE IN MULTI STEP TUNNELS
+        def skip_to_end():
+            qt = copy.deepcopy(q)
+            rt = 30 - qt.timer
+            for i in range(rt):
+                qt.update_flow_counter()
+            add_q(qt)
+        skip_to_end()
